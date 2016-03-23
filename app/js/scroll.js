8d712fa5dadef6ca3466811b6bad0d3ba416e3f6
kiga01 <ahinestrosa1984@gmail.com>
2016-03-23 13:33:01 -0500

    responsive adjustments

--- app/js/scroll.js
+++ app/js/scroll.js
@@ -3,93 +3,96 @@ var count = 0;
 var i = 0
 
 $(document).on('keydown', function (e) {
-    if (e.keyCode === 68) { // 68 is the letter D on the keyboard
-        count++;
-        if (count % 2 == 0 && countk < 10) {
-            $('.sprite1').removeClass('walk');
-            $('.sprite1').removeClass('iddle');
-            $('.sprite2').removeClass('walkk');
-            $('.sprite2').removeClass('iddlek');
-            $('.knight').css({'z-index': '1000'});
-            $('.orc').css({'z-index': '998'});
-            $('.sprite1').addClass('hurt');
-            $('.sprite2').addClass('attackk');
-            setTimeout(function () {
-                $('.sprite1').removeClass('hurt');
-                $('.sprite2').removeClass('attackk');
-            }, 150);
+    var windowWidth = $(window).width();
+    if (windowWidth >= 768) {
+        if (e.keyCode === 68) { // 68 is the letter D on the keyboard
+            count++;
+            if (count % 2 == 0 && countk < 10) {
+                $('.sprite1').removeClass('walk');
+                $('.sprite1').removeClass('iddle');
+                $('.sprite2').removeClass('walkk');
+                $('.sprite2').removeClass('iddlek');
+                $('.knight').css({'z-index': '1000'});
+                $('.orc').css({'z-index': '998'});
+                $('.sprite1').addClass('hurt');
+                $('.sprite2').addClass('attackk');
+                setTimeout(function () {
+                    $('.sprite1').removeClass('hurt');
+                    $('.sprite2').removeClass('attackk');
+                }, 150);
+            }
+            if (count % 2 == 0 && countk > 10) {
+                $('.sprite1').removeClass('walk');
+                $('.sprite1').removeClass('iddle');
+                $('.sprite1').addClass('defend');
+                setTimeout(function () {
+                    $('.sprite1').removeClass('defend');
+                }, 150);
+            }
+            if (count % 2 == 1 && countk < 10) {
+                $('.sprite1').removeClass('walk');
+                $('.sprite1').removeClass('iddle');
+                $('.sprite2').removeClass('walkk');
+                $('.sprite2').removeClass('iddlek');
+                $('.knight').css({'z-index': '1000'});
+                $('.orc').css({'z-index': '998'});
+                $('.sprite1').addClass('defend');
+                $('.sprite2').addClass('attackk');
+                setTimeout(function () {
+                    $('.sprite1').removeClass('defend');
+                    $('.sprite2').removeClass('attackk');
+                }, 150);
+            }
         }
-        if (count % 2 == 0 && countk > 10) {
-            $('.sprite1').removeClass('walk');
-            $('.sprite1').removeClass('iddle');
-            $('.sprite1').addClass('defend');
-            setTimeout(function () {
-                $('.sprite1').removeClass('defend');
-            }, 150);
-        }
-        if (count % 2 == 1 && countk < 10) {
-            $('.sprite1').removeClass('walk');
-            $('.sprite1').removeClass('iddle');
-            $('.sprite2').removeClass('walkk');
-            $('.sprite2').removeClass('iddlek');
-            $('.knight').css({'z-index': '1000'});
-            $('.orc').css({'z-index': '998'});
-            $('.sprite1').addClass('defend');
-            $('.sprite2').addClass('attackk');
-            setTimeout(function () {
-                $('.sprite1').removeClass('defend');
-                $('.sprite2').removeClass('attackk');
-            }, 150);
-        }
-    }
-    if (e.keyCode === 65) { // 65 is the letter A on the keyboard
-        countk++;
-        if (countk % 2 == 0 && countk < 10) {
-            $('.sprite1').removeClass('walk');
-            $('.sprite1').removeClass('iddle');
-            $('.sprite2').removeClass('walkk');
-            $('.sprite2').removeClass('iddlek');
-            $('.knight').css({'z-index': '998'});
-            $('.orc').css({'z-index': '1000'});
-            $('.sprite1').addClass('attack');
-            $('.sprite2').addClass('hurtk');
-            setTimeout(function () {
-                $('.sprite1').removeClass('attack');
-                $('.sprite2').removeClass('hurtk');
-            }, 150);
-        }
-        if (countk % 2 == 1 && countk < 10) {
-            $('.sprite1').removeClass('walk');
-            $('.sprite1').removeClass('iddle');
-            $('.sprite2').removeClass('walkk');
-            $('.sprite2').removeClass('iddlek');
-            $('.knight').css({'z-index': '998'});
-            $('.orc').css({'z-index': '1000'});
-            $('.sprite1').addClass('attack');
-            $('.sprite2').addClass('defendk');
-            setTimeout(function () {
-                $('.sprite1').removeClass('attack');
-                $('.sprite2').removeClass('defendk');
-            }, 150);
-        }
-        if (countk % 2 == 1 && countk > 10) {
-            $('.sprite1').removeClass('walk');
-            $('.sprite1').removeClass('iddle');
-            $('.sprite1').addClass('attack');
-            setTimeout(function () {
-                $('.sprite1').removeClass('attack');
-            }, 150);
-        }
-        if (countk == 10 && i == 0) {
-            i++;
-            $('.sprite2').removeClass('walkk');
-            $('.sprite2').removeClass('iddlek');
-            $('.sprite2').addClass('deadk');
-            setTimeout(function () {
+        if (e.keyCode === 65) { // 65 is the letter A on the keyboard
+            countk++;
+            if (countk % 2 == 0 && countk < 10) {
+                $('.sprite1').removeClass('walk');
+                $('.sprite1').removeClass('iddle');
+                $('.sprite2').removeClass('walkk');
+                $('.sprite2').removeClass('iddlek');
                 $('.knight').css({'z-index': '998'});
                 $('.orc').css({'z-index': '1000'});
-                $('.knight').hide();
-            }, 350);
+                $('.sprite1').addClass('attack');
+                $('.sprite2').addClass('hurtk');
+                setTimeout(function () {
+                    $('.sprite1').removeClass('attack');
+                    $('.sprite2').removeClass('hurtk');
+                }, 150);
+            }
+            if (countk % 2 == 1 && countk < 10) {
+                $('.sprite1').removeClass('walk');
+                $('.sprite1').removeClass('iddle');
+                $('.sprite2').removeClass('walkk');
+                $('.sprite2').removeClass('iddlek');
+                $('.knight').css({'z-index': '998'});
+                $('.orc').css({'z-index': '1000'});
+                $('.sprite1').addClass('attack');
+                $('.sprite2').addClass('defendk');
+                setTimeout(function () {
+                    $('.sprite1').removeClass('attack');
+                    $('.sprite2').removeClass('defendk');
+                }, 150);
+            }
+            if (countk % 2 == 1 && countk > 10) {
+                $('.sprite1').removeClass('walk');
+                $('.sprite1').removeClass('iddle');
+                $('.sprite1').addClass('attack');
+                setTimeout(function () {
+                    $('.sprite1').removeClass('attack');
+                }, 150);
+            }
+            if (countk == 10 && i == 0) {
+                i++;
+                $('.sprite2').removeClass('walkk');
+                $('.sprite2').removeClass('iddlek');
+                $('.sprite2').addClass('deadk');
+                setTimeout(function () {
+                    $('.knight').css({'z-index': '998'});
+                    $('.orc').css({'z-index': '1000'});
+                    $('.knight').hide();
+                }, 350);
+            }
         }
     }
 });
@@ -109,29 +112,34 @@ $(window).resize(function () {
     if (windowWidth < 768) {
         $('body').height(windowHeight);
     }
-    console.log('resize');
 });
 
 $(window).bind('scroll', function (e) {
-    parallaxScroll();
+    var windowWidth = $(window).width();
+    if (windowWidth >= 768) {
+        parallaxScroll();
+    }
 });
 
 $(window).scroll(function () {
-    $('.knight').css({'z-index': '998'});
-    $('.orc').css({'z-index': '1000'});
-    clearTimeout($.data(this, 'scrollTimer'));
-    $.data(this, 'scrollTimer', setTimeout(function () {
-        $('.sprite1').removeClass('walk');
-        $('.sprite1').addClass('iddle');
-        $('.sprite2').removeClass('walkk');
-        $('.sprite2').addClass('iddlek');
-        console.log("Haven't scrolled in 200ms!");
-    }, 1000));
-    $.data(this, 'scrollTimer', setTimeout(function () {
-        $('.sprite1').removeClass('iddle');
-        $('.sprite2').removeClass('iddlek');
-        console.log("Haven't scrolled in 4000ms!");
-    }, 2000));
+    var windowWidth = $(window).width();
+    if (windowWidth >= 768) {
+        $('.knight').css({'z-index': '998'});
+        $('.orc').css({'z-index': '1000'});
+        clearTimeout($.data(this, 'scrollTimer'));
+        $.data(this, 'scrollTimer', setTimeout(function () {
+            $('.sprite1').removeClass('walk');
+            $('.sprite1').addClass('iddle');
+            $('.sprite2').removeClass('walkk');
+            $('.sprite2').addClass('iddlek');
+            console.log("Haven't scrolled in 200ms!");
+        }, 1000));
+        $.data(this, 'scrollTimer', setTimeout(function () {
+            $('.sprite1').removeClass('iddle');
+            $('.sprite2').removeClass('iddlek');
+            console.log("Haven't scrolled in 4000ms!");
+        }, 2000));
+    }
 });
 
 function parallaxScroll() {
